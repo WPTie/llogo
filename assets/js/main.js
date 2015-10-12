bc2b5bfc3013b448c940d5c8d81f67dd78e5b5d0
Mohammad Ashar <ashar.worwox@gmail.com>
2015-10-13 00:55:35 +0500

    Documentation on the workflow in main.js is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,24 +5,38 @@
  * Main loading function
  */
 
-// loader
-
-// document.addEventListener("DOMContentLoaded", function(event) {
-// 	jQuery('#logo').loadgo();
-// });
-
-// TODO: Need documentation on the workflow here, what's going on.
-// I need to address an error, but only if I know what's going on here.
+/**
+ * This function runs after the page has been loaded
+ */
 jQuery(function($) {
-	jQuery(window).load(function($) { // makes sure the whole site is loaded
+	jQuery(window).load(function($) { 
+
+		jQuery('body > *:not(#aa_preloader)').addClass('aa_display--none');
+		
+		/**
+		 * 
+		 * Following script runs step by step when the site is loaded
+		 * 1. aa_timer variable is set to zero
+		 * 2. For loadgo plugin to run the animation
+		 * 	  'setprogress' function has to be called with progress in percentage
+		 * 3. A while loop runs, which provide progress parameter to 'setprogress'
+		 * 	  function
+		 * 4. After the loading animation, #aa_preloader fades out in 700ms and 
+		 *    the rest of the body appears
+		 *    
+		 */
 		var aa_timer = 0;
+
 		while( aa_timer < 100 ) {
-			setTimeout(function($) {
-				jQuery('#aa_logo').loadgo('setprogress', aa_timer);
-			}, 600);
+
+			jQuery('#aa_logo').loadgo('setprogress', aa_timer);
+
 			aa_timer++;
+
 		}
+
 		jQuery('#aa_preloader').delay(700).fadeOut('slow'); // will fade out the white DIV that covers the website.
-		jQuery('body').delay(700).css({'overflow':'hidden'});
+		jQuery('body > *:not(#aa_preloader)').delay(700).removeClass('aa_display--none');
+
 	});
 });
